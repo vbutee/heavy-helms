7153895f8d4696060a95b4807b1e4496534b65cd
ealeksandrov7 <evlogi@weichain.tech>
2025-02-28 13:12:54 +0200

    wip: game overview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import InfoBanner from "@/components/InfoBanner";
 import type { Character } from "@/types/player.types";
 import { CharacterGallery } from "@/components/home/character-gallery";
 import { CommunityStats } from "@/components/home/community-stats";
+import { GameIntroduction } from "@/components/home/game-introduction";
 
 // Sample character data - will be moved to a service later
 const characters: Character[] = [
@@ -93,8 +94,8 @@ export default function Home() {
   return (
     <div className="min-h-screen w-full overflow-y-auto">
       <CharacterGallery characters={characters} />
-	  <CommunityStats />
-
+      <GameIntroduction />
+      <CommunityStats />
     </div>
     // <>
     //   {/* Hero section */}
